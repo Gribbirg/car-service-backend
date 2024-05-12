e839ca4aaafdee8eaf39ffcc7f6fbc5816cc7a81
Alexander Gribkov <gribkovalexander@gmail.com>
2024-05-12 11:21:13 +0300

    Initial commit

---
+++ src/test/java/ru/mirea/carservicebackend/CarServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.carservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
